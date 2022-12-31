79532d0d5ff74617e457cc1fa73a5738caec0ee6
dhanashripatil205 <dhanashripatil205@gmail.com>
2022-12-31 11:20:53 +0530

    Check Attendence

---
+++ EmpWageNonStatic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageNonStatic
+{
+    public class EmpWageNonStatic
+    {
+        public void Employee()
+        {
+            int Is_Full_Time = 1;
+            Random R = new Random();
+            int empCheck = R.Next(2);
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent.");
+            }
+        }
+    }
+}
